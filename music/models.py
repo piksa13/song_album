2116abf6220623618c9531ce3802fa733ab08dd7
piksa13 <ania.piktas@gmail.com>
2017-02-01 23:41:53 +0000

    Add music app

---
+++ music/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here
+# it's a blueprint for data base, it consists of the classes
---
+++ music/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+# Create your views here.
+# takes the request from user and gives mostly websites
