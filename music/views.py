3f9fc1cc40040633ba2eaed0d9f000bc44e680b3
piksa13 <ania.piktas@gmail.com>
2017-02-02 17:15:35 +0000

    Adding template index.html

--- music/views.py
+++ music/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
+
 # Create your views here.
 # takes the request from user and gives mostly websites
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
+    #html=''
+    #for a in all_albums:
+        #url='/music/'+str(a.id)+'/'
+        #html += '<a href="'+ url +'">' + a.album_title +'</a><br>' # directing to music/str(q.id)/ on click
+    #return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
