b67dc0d5dab3bc6a3c17665b761e92df3dee890f
piksa13 <ania.piktas@gmail.com>
2017-02-02 17:44:53 +0000

    Adding detail.html and http404

--- music/views.py
+++ music/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from .models import Album
-
+from django.http import Http404
 
 # Create your views here.
 # takes the request from user and gives mostly websites
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album= Album.objects.get(pk = album_id)
+    except Album.DoesNotExist:
+        raise Http404('Hey bro this album doesn not exist.')
+    return render(request, 'music/detail.html', {'album' :album })
